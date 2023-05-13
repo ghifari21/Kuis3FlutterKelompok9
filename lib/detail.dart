e244541f4e0d6b5db9e843b09491b080bfb9b5a0
aliefabdillah <aliefmabdillah09@gmail.com>
2023-05-13 12:11:30 +0700

    create UI completed

--- lib/detail.dart
+++ lib/detail.dart
@@ -66,26 +66,67 @@ class DetailInterface extends StatelessWidget {
   Widget build(Object context) {
     return MaterialApp(
         home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail UMKM'),
+      ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           BlocBuilder<DetailCubit, DetailUmkm>(
             builder: (context, dataDetail) {
               context.read<DetailCubit>().fetchData(paramId);
-              return Center(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 20),
-                    ),
-                    Text("Nama: ${dataDetail.nama}"),
-                    Text("Detil: ${dataDetail.jenis}"),
-                    Text("Member Sejak: ${dataDetail.memberSejak}"),
-                    Text("Omzet perbulan: ${dataDetail.omzetPerBulan}"),
-                    Text("lama usaha: ${dataDetail.lamaUsaha}"),
+              /* var imageUrl = "";
+              if (dataDetail.jenis == "makanan/minuman") {
+                imageUrl = "https://img.okezone.com/content/2017/09/11/337/1773600/okezone-files-wajib-tahu-ini-ciri-ciri-warteg-asli-tegal-atau-bukan-Il9HFAsOJj.jpg";
+              }else if (dataDetail.jenis == "sembako"){
+                imageUrl = "https://radarpekalongan.id/wp-content/uploads/2023/01/toko-sembako-1536x1152.jpg";
+              }else if (dataDetail.jenis == "jasa") {
+                imageUrl = "https://www.maxmanroe.com/vid/wp-content/uploads/2019/11/Pengertian-Jasa-Adalah.jpg";
+              }else {
+                imageUrl = "https://cdn-2.tstatic.net/jakarta/foto/bank/images/metro-atom-pasar-baru-2.jpg";
+              } */
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /* Image.network(
+                      "https://www.maxmanroe.com/vid/wp-content/uploads/2019/11/Pengertian-Jasa-Adalah.jpg",
+                      height: 500,
+                      fit: BoxFit.cover,
+                    ), */
                     Text(
-                        "Jumlah pinjaman sukses: ${dataDetail.jumlahPinjamanSukses}")
-                  ]));
+                      dataDetail.nama,
+                      style:
+                          const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 24),
+                    Text(
+                      'Jenis UMKM: ${dataDetail.jenis}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Member sejak: ${dataDetail.memberSejak}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Omzet perbulan: ${dataDetail.omzetPerBulan}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Lama usaha: ${dataDetail.lamaUsaha}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Jumlah pinjaman sukses: ${dataDetail.jumlahPinjamanSukses}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              );
             },
           ),
         ]),
